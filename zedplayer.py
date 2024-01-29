0a67045765f5b8ec5a839b25589997cf63d792d9
LabeledZed <81475174+LabeledZed@users.noreply.github.com>
2024-01-29 20:41:15 +0200

    Update zedplayer.py

--- zedplayer.py
+++ zedplayer.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import os
 import eyed3
 from tkinter import *
@@ -6,6 +7,10 @@ from tkinter.messagebox import showerror
 from contextlib import redirect_stdout
 from datetime import timedelta
 import subprocess
+from threading import Thread
+
+import pypresence.exceptions
+from pypresence import Presence
 
 with redirect_stdout(open(os.devnull, "w")):
     import pygame
@@ -13,18 +18,11 @@ with redirect_stdout(open(os.devnull, "w")):
 playlist = []
 if getattr(sys, 'frozen', False):
     truepath = os.path.dirname(sys.executable)
-    try:
-        for i in range(len(sys.argv) - 1):
-            playlist.append(sys.argv[i+1])
-    except IndexError:
-        showerror("No attributes",
-                  "ZedPlayer has launched without attributes, therefore has nothing to play and will now exit.")
-        sys.exit()
+    for i in range(len(sys.argv) - 1):
+        playlist.append(sys.argv[i + 1])
 else:
     truepath = os.getcwd()
-    playlist.append(r"D:\Data\Audio\ATK Songs\Grindo, Anon, Mando - Dealers, Preza 2.mp3")
-    playlist.append(r"D:\Data\Audio\ATK Songs\Grindo, Lil Boxakos - Nigga 2.mp3")
-    playlist.append(r"D:\Data\Audio\ATK Songs\Grindo, Anon - Den Kanoume Xares.mp3")
+    playlist.append(r"D:\OldPersonal\AdobeAuditionOut\pond5-2.wav")
 
 win = Tk()
 
@@ -40,6 +38,72 @@ def center(query):
 center(win)
 win.configure(bg="#1a1a1a")
 win.title("ZedPlayer Alpha")
+isLooping = True
+pausestate = False
+epoch = int(time.time())
+
+
+def richpresence():
+    global pausestate, playingstr, epoch, isLooping
+    try:
+        rpc = Presence(1094371000029806592)
+        rpc.connect()
+        hasdiscord = True
+    except pypresence.exceptions.DiscordNotFound:
+        rpc = None
+        hasdiscord = False
+    if hasdiscord:
+        while isLooping:
+            if pausestate:
+                if "\nIn:" not in playingstr and "\nBy:" not in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerpause',
+                               small_text="v7-dev")
+                elif "\nIn:" in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               details="By: " + playingstr.split("By: ")[1].split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerpause',
+                               large_text="In: " + playingstr.split("In: ")[1].split("\n")[0],
+                               small_text="v7-dev")
+                elif "\nBy:" in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               details="By: " + playingstr.split("By: ")[1].split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerpause',
+                               small_text="v7-dev")
+            else:
+                if "\nIn:" not in playingstr and "\nBy:" not in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerplay',
+                               start=epoch - offset, end=epoch + trackend - offset,
+                               small_text="v7-dev")
+                elif "\nIn:" in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               details="By: " + playingstr.split("By: ")[1].split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerplay',
+                               large_text="In: " + playingstr.split("In: ")[1].split("\n")[0],
+                               start=epoch - offset, end=epoch + trackend - offset,
+                               small_text="v7-dev")
+                elif "\nBy:" in playingstr:
+                    rpc.update(state="Listening to: " + playingstr.split("\n")[0],
+                               details="By: " + playingstr.split("By: ")[1].split("\n")[0],
+                               large_image='zedplayer',
+                               small_image='zedplayerplay',
+                               start=epoch - offset, end=epoch + trackend - offset,
+                               small_text="v7-dev")
+
+
+def appexit():
+    global isLooping
+    isLooping = False
+    pygame.mixer.quit()
+    win.destroy()
+    os._exit(0)
+
 
 try:
     win.iconbitmap(sys._MEIPASS + "\\incl\\zedplayer.ico")
@@ -48,7 +112,7 @@ except AttributeError:
     win.iconbitmap(os.getcwd() + "\\incl\\zedplayer.ico")
     titleimg = PhotoImage(file=os.getcwd() + "\\incl\\zedplayer.png")
 win.resizable(False, False)
-win.protocol("WM_DELETE_WINDOW", lambda: sys.exit())
+win.protocol("WM_DELETE_WINDOW", appexit)
 
 dvar = DoubleVar()
 vvar = IntVar()
@@ -83,47 +147,55 @@ pygame.mixer.init()
 mixer = pygame.mixer.music
 musiccontrols = 0
 
-oldset = round((mixer.get_pos() / 1000) + 1)
 offset = 0
 canRun = False
 canNotRepeat = True
+canSeek = False
+isClicking = False
 
 
 def mplay():
-    global musiccontrols, trackend
+    global musiccontrols, trackend, pausestate, epoch, offset, tempepoch
     if elapse.get() == round(trackend):
         mstop()
+        pausestate = True
     if musiccontrols == 1:
         playbtn.configure(text="⏸")
         mixer.unpause()
+        epoch += int(time.time()) - tempepoch
+        pausestate = False
         musiccontrols = 0
     elif musiccontrols == 0:
         playbtn.configure(text="▶")
         mixer.pause()
+        tempepoch = int(time.time())
+        pausestate = True
         musiccontrols = 1
     else:
         playbtn.configure(text="⏸")
+        epoch = int(time.time())
         mixer.play()
+        pausestate = False
         musiccontrols = 0
 
 
 def mstop():
-    global musiccontrols, offset
+    global musiccontrols, offset, pausestate
     offset = 0
     mixer.stop()
+    playbtn.configure(text="▶")
+    pausestate = True
     musiccontrols = 3
 
 
 def mseek():
-    global offset, playlist, trackend, song, playingstr, canNotRepeat
+    global offset, playlist, trackend, song, playingstr, canNotRepeat, epoch
     offset = 0
     try:
-        mixer.queue(playlist[0])
-        if elapse.get() == round(trackend):
-            mstop()
-            mixer.play()
-        else:
-            mixer.set_pos(trackend)
+        mstop()
+        mixer.load(playlist[0])
+        mixer.play()
+        epoch = int(time.time())
         try:
             song = eyed3.load(playlist[0]).tag
         except AttributeError:
@@ -152,12 +224,17 @@ def mseek():
     except IndexError:
         canNotRepeat = False
         mixer.play()
+        epoch = int(time.time())
 
 
 try:
     song = eyed3.load(playlist[0]).tag
 except AttributeError:
     song = ""
+except IndexError:
+    showerror("No attributes",
+              "ZedPlayer has launched without attributes, therefore has nothing to play and will now exit.")
+    sys.exit()
 
 if not playlist[0] == "":
     trackend = pygame.mixer.Sound(playlist[0]).get_length()
@@ -185,7 +262,7 @@ if not playlist[0] == "":
 
 
 def setsizes():
-    global mixer, canRun
+    global mixer, canRun, epoch
     elapse.config(length=win.winfo_width() - elapsestr.winfo_width() - 8, to=trackend)
     if not canRun:
         volume.configure(length=win.winfo_height() - 32)
@@ -197,70 +274,76 @@ def setsizes():
         with redirect_stdout(open(os.getenv('APPDATA') + "\\ZedPlayer\\pid.old", "w")):
             print(os.getpid())
         canRun = True
+        Thread(target=richpresence).start()
         mixer.play()
+        epoch = int(time.time())
 
 
-def volumeadj(event):
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
+def volumeadj(*args):
     mixer.set_volume(volume.get() / 100)
     with redirect_stdout(open(os.getenv('APPDATA') + "\\ZedPlayer\\volume.info", "w")):
         print(volume.get())
 
 
 def infloop():
-    global trackend, oldset, offset, canRun, canNotRepeat
-    if elapse.get() == round(trackend):
-        if canNotRepeat:
-            mseek()
-        else:
-            mstop()
-            mplay()
-    elapsestr.configure(text=str(timedelta(seconds=elapse.get())).split(".")[0])
-    if not oldset == elapse.get() and not elapse.get() == round(trackend) and canRun:
-        mixer.set_pos(elapse.get())
-        offset += elapse.get() - oldset
-    oldset = round(mixer.get_pos() / 1000, 0) + offset
-    elapse.set((mixer.get_pos() / 1000) + offset)
-    win.after(1000, infloop)
+    global trackend, offset, canRun, canNotRepeat, isLooping, canSeek, isClicking
+    vmute = False
+    while isLooping:
+        if elapse.get() == round(trackend):
+            if canNotRepeat:
+                mseek()
+            else:
+                mstop()
+                mplay()
+        elapsestr.configure(text=str(timedelta(seconds=elapse.get())).split(".")[0])
+        if not canSeek:
+            if not isClicking:
+                if vmute:
+                    mixer.set_volume(volume.get() / 100)
+                    vmute = False
+                elapse.set((mixer.get_pos() / 1000) + offset)
+
+            else:
+                if not vmute:
+                    mixer.set_volume(0)
+                    vmute = True
+                try:
+                    mixer.set_pos(elapse.get())
+                except pygame.error:
+                    pass
+                offset = round(elapse.get()) - mixer.get_pos() / 1000
 
 
-def forw(event):
+def forw(*args):
     global offset, trackend
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
     if elapse.get() < trackend - 5:
         mixer.set_pos(elapse.get() + 5)
         offset += 5
 
 
-def backw(event):
-    global offset
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
+def backw(*args):
+    global offset, trackend
     if elapse.get() > 5:
         mixer.set_pos(elapse.get() - 5)
         offset -= 5
 
 
-def downw(event):
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
+def downw(*args):
     if not volume.get() < 5:
         volume.set(volume.get() - 5)
+    else:
+        volume.set(0)
 
 
-def upw(event):
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
+def upw(*args):
     if not volume.get() > 95:
         volume.set(volume.get() + 5)
+    else:
+        volume.set(100)
 
 
-def onrw(event):
+def onrw(*args):
     global canNotRepeat
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
     if canNotRepeat:
         canNotRepeat = False
         playing.configure(text="Now playing: " + playingstr + "\n🔁", fg="#ffffff")
@@ -269,12 +352,34 @@ def onrw(event):
         playing.configure(text="Now playing: " + playingstr + "\n", fg="#ffffff")
 
 
-def onspace(event):
-    with redirect_stdout(open(os.devnull, "w")):
-        print(event)
+def onspace(*args):
     mplay()
 
 
+def seekcheck(*args):
+    global canSeek
+    canSeek = True
+
+
+def nseekcheck():
+    global canSeek
+    while isLooping:
+        canSeek = False
+        time.sleep(0.01)
+
+
+def clickcheck(*args):
+    global isClicking
+    isClicking = True
+
+
+def nclickcheck(*args):
+    global isClicking
+    isClicking = False
+
+
+win.bind("<Button-1>", clickcheck)
+win.bind("<B1-ButtonRelease>", nclickcheck)
 win.bind("<space>", onspace)
 win.bind("<Left>", backw)
 win.bind("<Right>", forw)
@@ -292,6 +397,8 @@ if not os.path.isfile(os.getenv('APPDATA') + "\\ZedPlayer\\pid.old"):
 
 win.after(500, setsizes)
 volume.configure(command=volumeadj)
-infloop()
+elapse.bind("<Motion>", seekcheck)
+Thread(target=infloop).start()
+Thread(target=nseekcheck).start()
 win.focus()
 win.mainloop()
